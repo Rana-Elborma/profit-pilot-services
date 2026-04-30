8dd4d10fc70f93f2b729055f55006c721e2543ae
RNA <rranamah00@gmail.com>
2026-05-01 02:16:02 +0300

    fix the CORE to connect the backend

--- analytics-api/app/main.py
+++ analytics-api/app/main.py
@@ -21,7 +21,11 @@ app = FastAPI(title="Profit Pilot Analytics API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8081",
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
